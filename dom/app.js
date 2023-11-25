097402b6fcd1b54ecfd74513d2fc43458e178e70
Syed Mehar Ali Shah <syedzada12550@gmail.com>
2023-11-25 16:34:44 +0500

    more work

---
+++ dom/app.js
@@ -0,0 +1,76 @@
+
+// // **************************************************************** 1 ************************************************
+
+
+// function changeStyle() {
+//     let ul = document.querySelector("#ul");
+//     ul.style.fontFamily = "Arial, sans-serif";
+//     ul.style.color = "blue";
+//     ul.style.fontWeight = "700"
+
+
+// }
+
+// function changeInformation() {
+//     let ul = document.querySelector("#ul");
+
+//     let li1 = document.querySelector("#li1");
+//     let li2 = document.querySelector("#li2");
+//     let li3 = document.querySelector("#li3");
+
+//     li1.innerHTML = `<li > Nick Name: syed</li>`
+//     li2.innerHTML = `<li  >Favourites: Web Developer</li>`
+//     li3.innerHTML = `<li > HomeTown: Karachi </li>`
+// }
+
+
+// function showImage() {
+//     let image = document.createElement("img");
+
+//     let imagePath = 'https://global.discourse-cdn.com/business7/uploads/glideapps/original/2X/4/4936e0f28f79c8a2aa95582741091356d0ed61d8.jpeg';
+//     image.setAttribute("src", imagePath);
+
+//     document.body.appendChild(image);
+// }
+
+
+
+
+// // **************************************************************** 2 ********************************************************************************
+
+
+
+// let imageOfGoogle = document.querySelector("#imageOfGoogle");
+
+//     let changeImage = 'https://upload.wikimedia.org/wikipedia/commons/8/83/Yahoo%21_icon_%282009-2013%29.svg';
+//     imageOfGoogle.setAttribute("src", changeImage);
+
+
+// // **************************************************************** 3 ********************************************************************************
+
+
+    
+//     document.addEventListener('DOMContentLoaded', (event) => {
+//         let boldTextElements = document.querySelectorAll(".boldText");
+
+//         boldTextElements.forEach((element) => {
+//             element.addEventListener('click', () => {
+//                 element.style.color = "red";
+//             });
+//         });
+//     });
+
+// // **************************************************************** 4 ********************************************************************************
+
+
+//     let dropdown1 = document.querySelector("#dropdown1");
+//     let dropdown2 = document.querySelector("#dropdown2");
+//     let dropdown3 = document.querySelector("#dropdown3");
+//     let dropdown4 = document.querySelector("#dropdown4");
+// let allDropdwon = `all`
+
+//     let dropdown = document.querySelector("#dropdown");
+//     let dropDownBtn = document.querySelector("#dropDownBtn");
+//     dropDownBtn.addEventListener('click', () =>{
+//         console.log(dropdown1);
+//     })
--- functions/app.js
+++ functions/app.js
@@ -164,13 +164,36 @@
 // })
 
 
+
+
+
+
+
+// **************************************************** 10 ****************************************************************
+
+
+// function arguments(...arg) {
+//     console.log(...arg);
+// }
+// arguments("Hello", "World", 42);
+
+
+
+
+
+
+
+
+
+
+
 // **************************************************** 11 ****************************************************************
 
 
 
 // let numnber = [10, 30, 40, 50, 60, 70, 80, 200, 5];
 // let number1 = Math.max(...numnber);
-// console.log(number1);    
+// console.log(number1);
 
 
 
@@ -186,5 +209,301 @@
 
 
 
+// function value(width, height) {
+//     return width * height;
+// }
+// console.log("Area with values:" + 5 * 10);
+
+// function variable(varibleWidth, variableHeight) {
+//     let variableWidth1 = 5;
+//     let variableHeight2 = 10;
+//     let variableWidthHeight = variableWidth1 * variableHeight2;
+//     console.log("Area with variables:", variableWidthHeight);
+// }
+// variable();
 
 
+
+
+// **************************************************** 13 ****************************************************************
+
+
+// let sortButton = document.getElementById('btn');
+// let inputElement = document.getElementById('input');
+// let resultElement = document.getElementById('result');
+// sortButton.addEventListener('click', (event) => {
+//     event.preventDefault();
+//     let inputArray = inputElement.value.split(',');
+//     let sortedArray = inputArray.sort();
+//     resultElement.textContent = `Sorted Array: ${sortedArray}`;
+// });
+
+
+
+
+
+
+
+// **************************************************** 14 ****************************************************************
+
+
+
+// let sortButton = document.getElementById('btn');
+// let inputElement = document.getElementById('input');
+// let resultElement = document.getElementById('result');
+
+// sortButton.addEventListener('click', (event) => {
+//     event.preventDefault();
+
+//     let inputArray = inputElement.value.split(',').map(Number);
+
+//     function sumArray(numbers) {
+//         let total = 0;
+//         for (const num of numbers) {
+//             total += num;
+//         }
+//         return total;
+//     }
+
+//     let result = sumArray(inputArray);
+//     resultElement.textContent = `Sum of the array: ${result}`;
+// });
+
+
+// **************************************************** 15 ****************************************************************
+
+
+// let param = function inner() {
+//     return typeof inner;
+// }
+// alert(param());
+
+
+// function computePower(base, exponent) {
+//     return base ** exponent;
+// }
+
+
+
+// **************************************************** 16 ****************************************************************
+
+
+// const base = 2;
+// const exponent = 3;
+// const result = computePower(base, exponent);
+
+// console.log(`${base} ki power ${exponent} is ${result}`);
+
+
+
+
+
+
+
+// **************************************************** 17 ****************************************************************
+
+
+
+// function dice() {
+//     let random = Math.floor(Math.random() * 6) + 1;
+//     return random;
+//   }
+
+//   let result = dice();
+//   console.log(`The dice rolled: ${result}`);
+
+
+
+
+// **************************************************** 18 ****************************************************************
+
+
+// function number(number) {
+//     return parseInt(number.toString().split('').reverse().join(''));
+// }
+// let x = 32243;
+// let reverse = number(x);
+// console.log(`Example x: ${x}`);
+// console.log(`Expected Output: ${reverse}`);
+
+
+
+
+
+
+
+
+
+// **************************************************** 20 ****************************************************************
+
+
+
+// function para(str) {
+//     return str.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
+// }
+
+// const inputString = 'the quick brown fox';
+// const outputString = para(inputString);
+
+// console.log(`Example String: ${inputString}`);
+// console.log(`Expected Output: ${outputString}`);
+
+
+
+
+
+
+
+// **************************************************** 21 ****************************************************************
+
+
+// function word(str) {
+//     let words = str.split(' ');
+
+//     let longestWord = '';
+
+//     for (let i = 0; i < words.length; i++) {
+//         if (words[i].length > longestWord.length) {
+//             longestWord = words[i];
+//         }
+//     }
+
+//     return longestWord;
+// }
+// let inputString = 'Web Development Tutorial';
+// let longestWord = word(inputString);
+
+// console.log(`Example String: ${inputString}`);
+// console.log(`Expected Output: ${longestWord}`);
+
+
+
+
+
+
+
+
+
+// **************************************************** 22 ****************************************************************
+
+
+
+// function countVowels(str) {
+//     let vowels = 'aeiou';
+//     let count = 0;
+
+//     for (let i = 0; i < str.length; i++) {
+//         if (vowels.includes(str[i])) {
+//             count++;
+//         }
+//     }
+
+//     return count;
+// }
+
+// let inputString = 'The quick brown fox';
+// let numberOfVowels = countVowels(inputString);
+
+// console.log(`Example String: ${inputString}`);
+// console.log(`Expected Output: ${numberOfVowels}`);
+
+
+
+
+
+
+
+
+
+
+
+
+
+// **************************************************** 23 ****************************************************************
+
+
+
+
+// function getTypeOf(value) {
+//     return typeof value;
+// }
+
+// let exampleValues = [1, 'hello', true, { key: 'value' }, function () { }, undefined];
+
+// for (let i = 0; i < exampleValues.length; i++) {
+//     const value = exampleValues[i];
+//     console.log(`Type of ${value}: ${getTypeOf(value)}`);
+// }
+
+
+// **************************************************** 26 ****************************************************************
+
+
+
+// function calculateAge(birthYear, currentYear) {
+//     let age1 = currentYear - birthYear;
+//     let age2 = age1 - 1;
+
+//     console.log(`Age calculator`);
+
+//     console.log(`Current year :${currentYear}`);
+//     console.log(`Birth year :${birthYear1}`);
+//     console.log(`You are either ${age1} or ${age2} years old.`);
+
+//     console.log(`Age calculator`);
+
+//     console.log(`Current year :${currentYear}`);
+//     console.log(`Birth year :${birthYear2}`);
+//     console.log(`You are either ${age1} or ${age2} years old.`);
+// }
+// let birthYear1 = 1994;
+// let birthYear2 = 1997;
+
+// let currentYear = new Date().getFullYear();
+
+// calculateAge(birthYear2, currentYear);
+
+
+
+// **************************************************** 30 ****************************************************************
+
+
+
+// function calcCircumference(radius) {
+//     let circumference = 2 * Math.PI * radius;
+//     console.log(`The circumference is ${circumference.toFixed(2)}.`);
+// }
+
+// function calcArea(radius) {
+//     let area = Math.PI * Math.pow(radius, 2);
+//     console.log(`The area is ${area.toFixed(2)}.`);
+// }
+
+// let circleRadius = 5;
+
+// calcCircumference(circleRadius);
+// calcArea(circleRadius);
+
+
+
+
+// **************************************************** 29 ****************************************************************
+
+
+
+
+// function celsiusToFahrenheit(celsius) {
+//     let fahrenheit = (celsius * 9 / 5) + 32;
+//     console.log(`${celsius}°C is ${fahrenheit}°F.`);
+// }
+
+// function fahrenheitToCelsius(fahrenheit) {
+//     let celsius = (fahrenheit - 32) * 5 / 9;
+//     console.log(`${fahrenheit}°F is ${celsius}°C.`);
+// }
+
+// let celsiusTemperature = 30;
+// let fahrenheitTemperature = 86;
+
+// celsiusToFahrenheit(celsiusTemperature);
+// fahrenheitToCelsius(fahrenheitTemperature);
